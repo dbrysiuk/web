90a77abeeffaabacc283c10f81a1525b16cccf64
brysiuk <denis-brysiuk@hotmail.de>
2018-06-21 10:49:30 +0200

    test von startController

---
+++ advanced/frontend/views/start/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'My Yii Application';
+?>
+<div class="start-index">
+
+    <div class="jumbotron">
+		 <ul class="nav nav-pills">
+			<li class="active"><a href="#">Home</a></li>
+			  <li><a href="#">Student</a></li>
+			  <li><a href="#">Teacher</a></li>
+		</ul> 
+    </div>
+
+</div>
